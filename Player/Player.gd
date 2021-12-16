04bb5a709e26cb02d6b49db5b6cf00bbb0832a09
Wyat Soule <wsoule679@gmail.com>
2021-12-16 17:00:46 -0500

    add player animations with no code.

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,14 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else: 
