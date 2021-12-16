69951ac4619c4aeddf4520264bec9da1ecca7e35
Wyat Soule <wsoule679@gmail.com>
2021-12-16 11:04:28 -0500

    add new way of movement for player

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,17 +1,17 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 100
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed('ui_right'):
-		velocity.x = 4
-	elif Input.is_action_pressed('ui_left'):
-		velocity.x = -4
-	elif Input.is_action_pressed('ui_up'):
-		velocity.y = -4
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 4
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector 
 	else: 
-		velocity.x = 0
-		velocity.y = 0
-	move_and_collide(velocity)
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity * delta * MAX_SPEED)
